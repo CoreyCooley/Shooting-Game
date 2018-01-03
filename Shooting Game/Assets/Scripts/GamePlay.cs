631e626482f9c5bbdfe6fe43fb64bb1baf437461
Corey Cooley <corey.a.cooley@gmail.com>
2018-01-02 21:17:31 -0500

    Game Over Menu

--- Shooting Game/Assets/Scripts/GamePlay.cs
+++ Shooting Game/Assets/Scripts/GamePlay.cs
@@ -7,15 +7,19 @@ public class GamePlay : MonoBehaviour {
 
     public Text timerText;
     public Text scoreText;
+    public GameObject gameOverPanel;
 
+    private const int ROUND_TIME = 30;
     private int roundsShot = 0;
     private int enemyKilled = 0;
-
-    private int gameTime = 5;
+    
+    private int gameTime = 0;
+    public static bool gameIsPaused;
 
     // Use this for initialization
     void Start () {
-
+        gameTime = ROUND_TIME;
+        gameIsPaused = false;
         roundsShot = GunController.roundsShot;
 
         scoreText.text = enemyKilled.ToString() + " / " + roundsShot.ToString();
@@ -27,7 +31,7 @@ public class GamePlay : MonoBehaviour {
         int timeMinutes;
         int timeSeconds;
 
-        if (gameTime > 0)
+        if (gameTime >= 0)
         {
 
             timeMinutes = (int)(gameTime / 60);
@@ -38,7 +42,7 @@ public class GamePlay : MonoBehaviour {
         }
         else
         {
-            Debug.Log("Game is Over");
+            GameOver();
         }
     }
 
@@ -49,5 +53,28 @@ public class GamePlay : MonoBehaviour {
         enemyKilled = GunController.kills;
 
         scoreText.text = enemyKilled.ToString() + " / " + roundsShot.ToString();
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0;
+        gameIsPaused = true;
+        gameOverPanel.GetComponent<CanvasGroup>().alpha = 1;
+        gameOverPanel.GetComponent<CanvasGroup>().interactable = true;
+        gameOverPanel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        gameIsPaused = false;
+        gameOverPanel.GetComponent<CanvasGroup>().alpha = 0;
+        gameOverPanel.GetComponent<CanvasGroup>().interactable = false;
+        gameOverPanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        gameTime = ROUND_TIME;
+        GunController.Reset();
+        scoreText.GetComponent<Text>().text = enemyKilled.ToString() + " / " + roundsShot.ToString();
+        TimeCounterAction();
     }
 }
--- Shooting Game/Assets/Scripts/GunController.cs
+++ Shooting Game/Assets/Scripts/GunController.cs
@@ -18,32 +18,34 @@ public class GunController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        transform.LookAt(crossHair.transform);
-
-        float h = Input.GetAxis("Mouse X");
-        float v = Input.GetAxis("Mouse Y");
-
-        Vector3 crosshairMove = new Vector3(h, v, 0);
-        crossHair.transform.Translate(crosshairMove);
-
-        float x = crossHair.transform.position.x;
-        float y = crossHair.transform.position.y;
-
-        if (x > 10)
-            x = 10;
-        if (x < -10)
-            x = -10;
-        if (y > 6) 
-            y = 6;
-        if (y < -6)
-            y = -6;
-
-        crossHair.transform.position = new Vector3(x, y, 0);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!GamePlay.gameIsPaused)
         {
-            ShootAction();
+            transform.LookAt(crossHair.transform);
+
+            float h = Input.GetAxis("Mouse X");
+            float v = Input.GetAxis("Mouse Y");
+
+            Vector3 crosshairMove = new Vector3(h, v, 0);
+            crossHair.transform.Translate(crosshairMove);
+
+            float x = crossHair.transform.position.x;
+            float y = crossHair.transform.position.y;
+
+            if (x > 10)
+                x = 10;
+            if (x < -10)
+                x = -10;
+            if (y > 6)
+                y = 6;
+            if (y < -6)
+                y = -6;
+
+            crossHair.transform.position = new Vector3(x, y, 0);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ShootAction();
+            }
         }
     }
 
